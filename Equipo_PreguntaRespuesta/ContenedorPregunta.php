088933d00362a28d12d7172d168c15fb54cc977b
Ad4n <jesusadanorozcop@outlook.com>
2024-10-11 10:05:03 -0700

    El contenedor de pregunta trae las preguntas de la base de datos

--- Equipo_PreguntaRespuesta/ContenedorPregunta.php
+++ Equipo_PreguntaRespuesta/ContenedorPregunta.php
@@ -39,26 +39,63 @@
 </div>
 
 <!-------------------------------------------Parte de Adan----------------------------------->
-    <div class="container mt-5">
-        <div class="card">
-            <div class="card-body">
-                <div class="row justify-content-between">
-                    <p class="card-text col-5">Autor: Anónimo</p>
-                    <p class="card-text col-5 text-end">1:25 pm</p>
-                    <hr>
-                </div>
-                <p class="card-text"  style="margin-top: -3%;"><h2><B>¿Qué es una base de datos?</B></h2></p>
-                <div class="row justify-content-between">
-                    <p class="card-text col-8">Tengo la duda que en si que es una base de datos como NoSQL y SQL cual serian sus diferencias...</p>
-                    <button type="image" class="btn col-3" style="margin-top: -10%;" onclick="window.open('pregunta_detalle.php', '_blank')">
-                        <img class="w-75" src="https://cdn-icons-png.flaticon.com/512/159/159604.png" alt="" >
-                    </button> 
-                </div>
-                
-                
-            </div>
-        </div>
-    </div>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sistemajornada";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT Id_Pregunta, Id_autor, id_Tema, Pregunta, Contexto, Hora, Fecha, Estado FROM pregunta";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<div class= 'container mt-5'>";
+            echo "<div class='card-body'>";
+                echo "<div class='row justify-content-between'>";
+                    echo "<p class='card-text col-5'>Autor: " . $row["Id_autor"] . "</p>";
+                    echo "<p class='card-text col-5 text-end'>" . $row["Hora"] . "</p>";
+                    echo "<hr>";
+                echo "</div>";
+                echo "<p class='card-text'  style='margin-top: -3%;'><h2><B>" . $row["Pregunta"] . "</B></h2></p>";
+                echo "<div class='row justify-content-between'>";
+                    echo "<p class='card-text col-8'>" . $row["Contexto"] . "</p>";
+                    echo "<button type='image' class='btn col-3' style='margin-top: -10%;' onclick='window.open('pregunta_detalle.php', '_blank')'>";
+                        echo "<img id='eyeimg' src='https://cdn-icons-png.flaticon.com/512/159/159604.png' alt='' >";
+                    echo "</button> ";
+                echo "</div>";
+            echo "</div>";
+        echo "</div>";
+    echo "</div>";
+    }
+} else {
+    echo "0 resultados";
+}
+$conn->close();
+?>
+    
+
+
+
+
+
+
+<!-- 
+
+
+-->
+
+
+    
 
     <!-- Bootstrap 5 JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
